b9f23aaffa2e1d79337f878ddb6277fa60efcbd1
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-01-12 23:42:46 +0100

    diferenciar beans con @Primary

--- src/main/java/com/eldin/spring/basic/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/QuickSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.eldin.spring.basic.springin5steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort ( int[] numbers){
 
