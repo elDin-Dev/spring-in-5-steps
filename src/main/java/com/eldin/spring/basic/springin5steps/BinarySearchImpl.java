12000c74b6dfad1f3d3c125b9c1db4654a449771
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-01-13 00:09:51 +0100

    diferenciar beans by qualifier

--- src/main/java/com/eldin/spring/basic/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/BinarySearchImpl.java
@@ -1,12 +1,14 @@
 package com.eldin.spring.basic.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
     @Autowired
+    @Qualifier("quick")
     private SortAlgorithm sortAlgorithm;
 
 
