25b1d1dba668f14076bcea22a6a5de8989a10d32
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-01-12 23:50:10 +0100

    wiring por setter y no constructor

--- src/main/java/com/eldin/spring/basic/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/BinarySearchImpl.java
@@ -9,10 +9,6 @@ public class BinarySearchImpl {
     @Autowired
     private SortAlgorithm sortAlgorithm;
 
-    public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-        super();
-        this.sortAlgorithm = sortAlgorithm;
-    }
 
     public int binarySearch(int[] numbers, int numberToSearchFor){
 
