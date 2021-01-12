05cded6c23f4008e2596c39ea4f0d70e1c899486
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-01-12 23:32:41 +0100

    añadir componentes y contexto

--- src/main/java/com/eldin/spring/basic/springin5steps/BubleSortAlgorithm.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/BubleSortAlgorithm.java
@@ -1,5 +1,9 @@
 package com.eldin.spring.basic.springin5steps;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubleSortAlgorithm implements SortAlgorithm {
     public int[] sort ( int[] numbers){
 
--- src/main/java/com/eldin/spring/basic/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.eldin.spring.basic.springin5steps;
 
+import org.springframework.stereotype.Component;
+
+
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort ( int[] numbers){
 
