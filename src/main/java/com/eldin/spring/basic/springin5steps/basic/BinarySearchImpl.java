9875f3fa049f25d8e2c65ca445232c3c469d3687
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-01-14 23:01:20 +0100

    lifecycle for beans. postconstruct and predestroy

--- src/main/java/com/eldin/spring/basic/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/basic/BinarySearchImpl.java
@@ -1,19 +1,30 @@
 package com.eldin.spring.basic.springin5steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("quick")
     private SortAlgorithm sortAlgorithm;
 
+    @PostConstruct
+    public void postConstruct(){
+        logger.info("postConstruct: {}",this);
+    }
 
     public int binarySearch(int[] numbers, int numberToSearchFor){
 
@@ -22,6 +33,11 @@ public class BinarySearchImpl {
         System.out.println(sortedNumbers);
 
         return 3;
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        logger.info("preDestroy: {}",this);
     }
 
 
