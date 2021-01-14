8666ad16d440506668a282146f5ee3423c0314d2
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-01-14 23:38:53 +0100

    auto close del context con try(){...}

--- src/main/java/com/eldin/spring/basic/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/eldin/spring/basic/springin5steps/SpringIn5StepsBasicApplication.java
@@ -12,19 +12,16 @@ public class SpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
 
+		try (AnnotationConfigApplicationContext applicationContext=
+				 new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);) {
+			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 
-		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+			int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
 
-		ApplicationContext applicationContext=
-				 new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+			//System.out.println(binarySearch);
+			System.out.println(result);
 
-		int result = binarySearch.binarySearch(new int[] {12,4,6}, 3);
-
-		//System.out.println(binarySearch);
-		System.out.println(result);
-
-
+		}
 
 	}
 
